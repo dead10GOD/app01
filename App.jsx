eea45dd3f8493152bef55170cdbdb10b75779c98
Bornin112004 <155297095+Bornin112004@users.noreply.github.com>
2024-12-02 23:10:18 +0530

    navigation error to welcome screen fixed

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./src/screen/HomeScreen";
 import LoginScreen from "./src/screen/LoginScreen";
 import SignupScreen from "./src/screen/SignupScreen";
+import WelcomeScreen from "./src/screen/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,7 @@ const App = () => {
         <Stack.Screen name={"HOME"} component={HomeScreen} />
         <Stack.Screen name={"LOGIN"} component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignupScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
